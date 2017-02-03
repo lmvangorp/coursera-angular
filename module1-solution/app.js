dca540d6f09402292adaa4f5d561e9d97c9e31c2
Lisa Van Gorp <lmvangorp@gmail.com>
2017-02-03 12:15:22 -0500

    New Folder

---
+++ module1-solution/app.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+    
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope){
+        $scope.lunchItems = "";
+        $scope.message = "";
+
+        $scope.parseItems = function(){
+            var lunch = $scope.lunchItems;
+            var lunchList = lunch.split(' ');
+
+            if(lunch == ""){
+                $scope.message = "Please enter data first";
+            }
+            else if(lunchList.length <= 3){
+                    $scope.message = "Enjoy!";
+                }
+            else{
+                    $scope.message = "Too much!";
+                }
+        };
+    }
+})();
